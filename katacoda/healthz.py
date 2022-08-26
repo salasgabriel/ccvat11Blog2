6ca77e0eed0e1146335e3d8e8d87bd3f11c676c5
salasgabriel <109995706+salasgabriel@users.noreply.github.com>
2022-08-26 09:17:24 -0300

    yyuy78

--- katacoda/healthz.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def ready(request):
-    return HttpResponse("OK", content_type="text/plain")
-
-def alive(request):
-    return HttpResponse("OK", content_type="text/plain")
